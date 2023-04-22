4332a83612192cfb0c8dedca468cd7da64e378f5
oscarcortez <oscar.cortez@jalasoft.com>
2023-04-22 10:14:49 -0400

    new component created estado-cuenta generated by cli ng generate component name_component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NuevaTransferenciaComponent } from './nueva-transferencia/nueva-transferencia.component';
+import { EstadoCuentaComponent } from './estado-cuenta/estado-cuenta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NuevaTransferenciaComponent
+    NuevaTransferenciaComponent,
+    EstadoCuentaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/estado-cuenta/estado-cuenta.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EstadoCuentaComponent } from './estado-cuenta.component';
+
+describe('EstadoCuentaComponent', () => {
+  let component: EstadoCuentaComponent;
+  let fixture: ComponentFixture<EstadoCuentaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EstadoCuentaComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EstadoCuentaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
